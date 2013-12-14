fbfa27c145a5de37f73100112828909e2744f334
buildslave <buildslave@buildbot.tl.intel.com>
2013-12-14 03:07:35 +0000

    "Automatically revert commits from previous weekly release"
    Change-Id: Idd453ab25873841cbdf5f6294a66fe94ce583483
    Signed-off-by: buildslave <buildslave@buildbot.tl.intel.com>

--- src/com/android/cellbroadcastreceiver/CellBroadcastReceiver.java
+++ src/com/android/cellbroadcastreceiver/CellBroadcastReceiver.java
@@ -35,12 +35,10 @@ import android.util.Log;
 
 import com.android.internal.telephony.ITelephony;
 import com.android.internal.telephony.cdma.sms.SmsEnvelope;
-import com.android.internal.telephony.IccCardConstants;
-import com.android.internal.telephony.TelephonyIntents;
 
 public class CellBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "CellBroadcastReceiver";
-    static final boolean DBG = false;
+    static final boolean DBG = true;    // STOPSHIP: change to false before ship
 
     private static final String GET_LATEST_CB_AREA_INFO_ACTION =
             "android.cellbroadcastreceiver.GET_LATEST_CB_AREA_INFO";
@@ -55,12 +53,12 @@ public class CellBroadcastReceiver extends BroadcastReceiver {
 
         String action = intent.getAction();
 
-        if (TelephonyIntents.ACTION_SIM_STATE_CHANGED.equals(action)) {
-            String stateExtra = intent.getStringExtra(IccCardConstants.INTENT_KEY_ICC_STATE);
-            if (stateExtra != null
-                    && IccCardConstants.INTENT_VALUE_ICC_LOADED.equals(stateExtra)) {
-                startConfigService(context);
-            }
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
+            if (DBG) log("Registering for ServiceState updates");
+            TelephonyManager tm = (TelephonyManager) context.getSystemService(
+                    Context.TELEPHONY_SERVICE);
+            tm.listen(new ServiceStateListener(context.getApplicationContext()),
+                    PhoneStateListener.LISTEN_SERVICE_STATE);
         } else if (Intent.ACTION_AIRPLANE_MODE_CHANGED.equals(action)) {
             boolean airplaneModeOn = intent.getBooleanExtra("state", false);
             if (DBG) log("airplaneModeOn: " + airplaneModeOn);
@@ -202,6 +200,28 @@ public class CellBroadcastReceiver extends BroadcastReceiver {
             Log.w(TAG, "phone.getActivePhoneType() failed", e);
         }
         return isCdma;
+    }
+
+    private static class ServiceStateListener extends PhoneStateListener {
+        private final Context mContext;
+        private int mServiceState = -1;
+
+        ServiceStateListener(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public void onServiceStateChanged(ServiceState ss) {
+            int newState = ss.getState();
+            if (newState != mServiceState) {
+                Log.d(TAG, "Service state changed! " + newState + " Full: " + ss);
+                mServiceState = newState;
+                if (newState == ServiceState.STATE_IN_SERVICE ||
+                        newState == ServiceState.STATE_EMERGENCY_ONLY) {
+                    startConfigService(mContext);
+                }
+            }
+        }
     }
 
     private static void log(String msg) {
